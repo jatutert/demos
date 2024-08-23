b449f5b116b8297d0091bccd93529c42d60025ec
John Tutert <jatutert@gmail.com>
2024-08-23 18:52:54 +0200

    Flask demo update

--- Docker/Dockerfiles/FLASK/flask-demo-run.sh
+++ Docker/Dockerfiles/FLASK/flask-demo-run.sh
@@ -1,18 +1,37 @@
 #! /bin/bash
-# 
-# default
-# https://hackmd.io/@pmanzoni/r1uWcTqfU
-# docker run -p 8888:5000 -d --name flaskdemo flask-demo:v100
-# meerdere talen demo 
-docker run -p 9001:5000 -d --name flask-demo-de flask-demo-de:v100
-docker run -p 9002:5000 -d --name flask-demo-fr flask-demo-fr:v100
-docker run -p 9003:5000 -d --name flask-demo-it flask-demo-it:v100
-docker run -p 9004:5000 -d --name flask-demo-nl flask-demo-nl:v100
-docker run -p 9005:5000 -d --name flask-demo-uk flask-demo-uk:v100
 #
+# Script for starting multiple demo docker images 
+#
+# Author: John Tutert
+#
+# Based on default flash demo https://hackmd.io/@pmanzoni/r1uWcTqfU
+#
+# For Personal or educational use only 
+#
+docker run -p 9001:5000 -d --name website-one flask-demo:v100
+docker run -p 9002:5000 -d --name website-two flask-demo:v100
+docker run -p 9003:5000 -d --name website-three flask-demo:v100
+docker run -p 9004:5000 -d --name website-four flask-demo:v100
+docker run -p 9005:5000 -d --name website-five flask-demo:v100
+#
+# meerdere talen demo 
+# docker run -p 9001:5000 -d --name flask-demo-de flask-demo-de:v100
+# docker run -p 9002:5000 -d --name flask-demo-fr flask-demo-fr:v100
+# docker run -p 9003:5000 -d --name flask-demo-it flask-demo-it:v100
+# docker run -p 9004:5000 -d --name flask-demo-nl flask-demo-nl:v100
+# docker run -p 9005:5000 -d --name flask-demo-uk flask-demo-uk:v100
+#
+# Ophalen IP address
 ip_address=$(ip addr show eth1 | grep 'inet\b' | awk '{print $2}' | cut -d/ -f1)
-echo "Deutsch op poort 9001" $ip_address
-echo "Francais op poort 9002" $ip_address
-echo "Italiano op poort 9003" $ip_address
-echo "Nederlands op poort 9004" $ip_address
-echo "English op poort 9005" $ip_address
+#
+echo "Website One at port 9001" $ip_address
+# echo "Deutsch op poort 9001" $ip_address
+echo "Website Two at port 9002" $ip_address
+# echo "Francais op poort 9002" $ip_address
+echo "Website Three at port 9003" $ip_address
+# echo "Italiano op poort 9003" $ip_address
+echo "Website Four at port 9004" $ip_address
+# echo "Nederlands op poort 9004" $ip_address
+echo "Website Five at port 9005" $ip_address
+# echo "English op poort 9005" $ip_address
+
--- Docker/Dockerfiles/FLASK/flask-image-build.sh
+++ Docker/Dockerfiles/FLASK/flask-image-build.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
-# default flash demo 
-# https://hackmd.io/@pmanzoni/r1uWcTqfU
+#
+# Script for building Flask demo Docker Image
+#
+# Author: John Tutert
+# 
+# Based on default flash demo https://hackmd.io/@pmanzoni/r1uWcTqfU
+#
+# For Personal or educational use only 
+#
+docker build -f /home/vagrant/docker/flask-demo/flask-demo-dkr-file -t flask-demo:v100 .
 # docker build -f ./flask-demo-dkr-file -t flask-demo:v100 .
+#
+#
 # Meerdere talen demo 
-docker build -f ./flask-demo-dkr-file-de -t flask-demo-de:v100 .
-docker build -f ./flask-demo-dkr-file-fr -t flask-demo-fr:v100 .
-docker build -f ./flask-demo-dkr-file-it -t flask-demo-it:v100 .
-docker build -f ./flask-demo-dkr-file-nl -t flask-demo-nl:v100 .
-docker build -f ./flask-demo-dkr-file-uk -t flask-demo-uk:v100 .
+# docker build -f ./flask-demo-dkr-file-de -t flask-demo-de:v100 .
+# docker build -f ./flask-demo-dkr-file-fr -t flask-demo-fr:v100 .
+# docker build -f ./flask-demo-dkr-file-it -t flask-demo-it:v100 .
+# docker build -f ./flask-demo-dkr-file-nl -t flask-demo-nl:v100 .
+# docker build -f ./flask-demo-dkr-file-uk -t flask-demo-uk:v100 .
