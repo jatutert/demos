33a2d7b9afc8fb010ddad1799b3201edf350a3c3
John Tutert <jatutert@gmail.com>
2026-03-23 22:15:23 +0100

    Bugfixes

--- Windows/VirtualHarddisk/Create-VMDK/WS22-From-ISO/Eigen-VMDK-Powershell/VMDKPath-Virtual-Machines/W22-DTC-Create-VMDK-V004.cmd
+++ Windows/VirtualHarddisk/Create-VMDK/WS22-From-ISO/Eigen-VMDK-Powershell/VMDKPath-Virtual-Machines/W22-DTC-Create-VMDK-V004.cmd
@@ -15,7 +15,7 @@
 ::
 ::
 ::
-Powershell -file "D:\OneDrive\OneDrive - Saxion\Repository-Playground\Development\GitHub-GitDesktop\Demos\Windows\VirtualHarddisk\Create-VHD\WS22-From-ISO\Powershell\VHDPath-Virtual-Machines"\W22-DTC-Create-VHD-V004.ps1
+Powershell -file "D:\OneDrive\OneDrive - Saxion\Repository-Playground\Development\GitHub-GitDesktop\Demos\Windows\VirtualHarddisk\Create-VHD\WS22-From-ISO\Powershell\VHDPath-Virtual-Machines"\W22-DTC-Create-VHD-V005.ps1
 ::
 @echo Conversie van VHD naar VMDK gestart ... 
 "C:\Program Files\StarWind Software\StarWind V2V Converter\V2V_ConverterConsole.exe" convert in_file_name="D:\Virtual-Machines\Oracle-VM-Virtualbox\Windows\Server\W22-DTC-S-LAB-001.VHD" out_file_name="D:\Virtual-Machines\Oracle-VM-Virtualbox\Windows\Server\W22-DTC-S-LAB-001.VMDK" out_file_type=ft_vmdk_ws_growable
